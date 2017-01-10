253cb7516c6a1dfde8db012382f08782acad1a10
vibecode <rumashkin@gmail.com>
2017-01-10 17:00:56 +0100

    add firebase config

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import Firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+    Firebase.initializeApp({
+      apiKey: 'AIzaSyBmKu55SGQCgDaip20oxwNYilm4NCxjZbw',
+      authDomain: 'authmobile-442b8.firebaseapp.com',
+      databaseURL: 'https://authmobile-442b8.firebaseio.com',
+      storageBucket: 'authmobile-442b8.appspot.com',
+      messagingSenderId: '742330710970'
+    });
+  }
+
   render() {
     return (
         <View>
-          <Header headerText="Authentication">
-            <Text>An App</Text>
-          </Header>
+          <Header headerText="Authentication"/>
+          <Text>An App</Text>
         </View>
     )
   }
